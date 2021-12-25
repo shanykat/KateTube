6e051f9bbac9c594b4a83e871b8f05cd65ea88c2
shanykat <ekkadd27@gmail.com>
2021-12-25 18:47:30 +0300

    Отправка комментариев в базу данных ver.5

--- app/src/main/java/org/o7planning/katetube/MainActivity.java
+++ app/src/main/java/org/o7planning/katetube/MainActivity.java
@@ -1,6 +1,7 @@
 package org.o7planning.katetube;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,7 +9,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,12 +22,20 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+    DatabaseReference myRef = database.getReference("comments");
 
-    Button but_back1, but_next1, bttest;
+    Button but_back1, but_next1, nBut_comment;
+    EditText nEditTextComment;
+
+    ArrayList <String> comments = new ArrayList<>();
+
+    private  static int MAX_COMMENT_LENGTH=50;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +50,62 @@ public class MainActivity extends AppCompatActivity {
             public void onReady(@NonNull YouTubePlayer youTubePlayer) {
                 String videoId = "lBJyaIR1mlw";
                 youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+
+        nBut_comment = findViewById(R.id.but_comment);
+        nEditTextComment = findViewById(R.id.comment);
+
+        nBut_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String comt = nEditTextComment.getText().toString();
+
+                if (comt.equals("") ){
+
+                    Toast.makeText(getApplicationContext(),"Введите комментарий", Toast.LENGTH_SHORT). show();
+                    return;
+
+                }
+
+                if (comt.length() > MAX_COMMENT_LENGTH) {
+
+                    Toast.makeText(getApplicationContext(),"Превышен лимит символов", Toast.LENGTH_SHORT). show();
+                    return;
+
+                }
+
+                myRef.push().setValue(comt);
+                nEditTextComment.setText("");
+            }
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot datasnapshot, @Nullable String previousChildName) {
+                String comt = datasnapshot.getValue(String.class);
+                comments.add(comt);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
